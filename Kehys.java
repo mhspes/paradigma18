5731e176a019acc6fcc528594c03fed34d4a8733
pajeet063 <39622164+mhspes@users.noreply.github.com>
2018-07-25 21:17:44 +0300

    Add files via upload

--- Kehys.java
+++ Kehys.java
@@ -1,50 +1,70 @@
-import java.awt.*;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+/* JPanel-luokan toteuttava luokka kuvion piirt�mist� varten
+ * */
 
-public class Kehys extends JPanel{
-		int a;
-		int b;
-		
-		public Kehys() {}
-		
-		//jotta pinoa ei tarvitse lukea niin pitkään kerralla, muodostaa parista x, y Point-tyypin
-		protected Point muodostaKoordinaatti(Object n1, Object n2){
-			n1 = (int)a;
-			n2 = (int)b;
-			Point A = new Point(a, b);
-			return A;
-		}
-		
-		public void paint (Graphics g){
-			
-			//kun väri on asetettu, kaikki tämän jälkeen piirrettävät tulee sillä värillä
-			g.setColor(Color.RED);
-			Point A = new Point(10, 10);
-			Point B = new Point(500, 200);
-			Point C = new Point(100, 80);
-			int numero = 2;
-			switch (numero) {
-		      case 3: //viiva
-		    	  	g.drawLine(A.x, A.y, B.x, B.y);
-		    	  	break;
-		        case 1: //ympyrä
-		        	g.drawOval(A.x, A.y, 50, 50); // koska on ympyrä, voidaan käyttää ovaalia mutta säteen (sade) on oltava sama sekä korkeudelle että leveydelle
-		        	break;
-		        case 2: //piste
-		        	g.drawOval(A.x, A.y, 10, 10);
-		        	g.fillOval(A.x, A.y, 10, 10); //täytetään ympyrää värillä, jotta sen näkisi
-		        	break;
-		        case 4: //kolmio
-		        	g.drawLine(A.x, A.y, B.x, B.y);
-					g.drawLine(B.x, B.y, C.x, C.y);
-					g.drawLine(C.x, C.y, A.x, A.y);
-					break;
-		        case 5: //clear
-		        	g.fillRect(0, 0, 1000, 1000);
-		        	break;
-			}
-		}
-		
-		
+import java.awt.*;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+class Kehys extends JPanel{
+  protected Point A2;        // Koordinaattiparit
+  protected Point B2;
+  protected Point C2;
+  protected String colour;
+  protected String shape;
+  
+  /* Metodi koordinaattien, v�rin ja muodon 
+   * siirt�miseksi Kehys-oliolle
+   * */
+  public void param(Point A2, Point B2, Point C2, String colour, String shape){
+    this.A2=A2;
+    this.B2=B2;
+    this.C2=C2;
+    this.colour=colour;
+    this.shape=shape;
+  }
+  /* Metodi kuvion piirt�miseksi JFrame-ikkunaan
+   * 
+   * */
+  protected void paintComponent(Graphics g){
+    
+    // V�rin valinta
+    switch (colour){
+      case "red":
+        g.setColor(Color.RED);
+        break;
+      case "blue":
+        g.setColor(Color.BLUE);
+        break;
+      case "green":
+        g.setColor(Color.GREEN);
+        break;
+      case "black":
+        g.setColor(Color.BLACK);
+        break;
+      case "yellow":
+        g.setColor(Color.YELLOW);
+        break;
+    }
+    // Kuvion valinta
+    switch(shape){
+      case "point":
+        g.drawOval(A2.x, A2.y, 5, 5);
+        g.fillOval(A2.x, A2.y, 5, 5); 
+        break;
+      case "line":
+        g.drawLine(A2.x, A2.y, B2.x, B2.y);
+        break;
+      case "circle":
+        g.drawOval(A2.x, A2.y, B2.x, B2.x);
+        break;
+      case "triangle":
+        g.drawLine(A2.x, A2.y, B2.x, B2.y);
+        g.drawLine(B2.x, B2.y, C2.x, C2.y);
+        g.drawLine(C2.x, C2.y, A2.x, A2.y);
+        break;
+      case "clear":
+        g.clearRect(0, 0, 1000, 1000);
+        break;
+    }
+  }
 }
